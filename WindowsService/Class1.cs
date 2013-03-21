daebf8ab837c6785d7bc9723ca57be8d7056babb
Shaun Luttin <shaunluttin@gmail.com>
2013-03-21 10:18:28 -0700

    Complete steps 1-6 of HowTo: Create Services Programmatically (http://msdn.microsoft.com/en-us/library/76477d2t.aspx), because we are creating a Windows Service to act as the IMAP email alert system.

---
+++ WindowsService/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UserService1 : System.ServiceProcess.ServiceBase
+{
+    public static void Main()
+    {
+        System.ServiceProcess.ServiceBase.Run(new UserService1());
+    }
+    public UserService1()
+    {
+        this.ServiceName = "MyService2";
+        this.CanStop = true;
+        this.CanPauseAndContinue = true;
+        this.AutoLog = true;
+    }
+    protected override void OnStart(string[] args)
+    {
+        // Insert code here to define processing.
+    }
+}
