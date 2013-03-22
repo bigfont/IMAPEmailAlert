da859562365de9c293f67756ae08668834d3b5f5
Shaun Luttin <shaunluttin@gmail.com>
2013-03-22 11:31:16 -0700

    Add back EmailAlertTask directory after having removed the private data.

---
+++ EmailAlertTask/EmailAlertTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailAlertTask
+{
+    class EmailAlertTask
+    {
+        static void Main(string[] args)
+        {            
+            try
+            {
+                EmailAlertClassLibrary.ImapAndSmtp imapAndSmtp = new EmailAlertClassLibrary.ImapAndSmtp();
+       
+                // Paste PrivateUserData.txt contents here.
+                // eg. imapAndSmtp.CountEmailsAndSendAlert("icoEmail", "icoPassword", "secondaryEmail");
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+            }
+        }
+    }
+}
